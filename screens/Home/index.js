15d4828590ce5c489b641d93b816826376ab5de9
Vitu Mafeni <mafeniblessed@gmail.com>
2023-12-13 02:12:48 +0900

    added carousel

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,19 +1,29 @@
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, SafeAreaView} from "react-native";
 import Search from "../../components/Home/search";
+import CarouselCards from "../../components/Home/carousel/CarouselCards";
 
 export default function Home() {
     return (
         <View style={styles.homeContainer}>
             {/*<Text>Homescreen</Text>*/}
             <Search/>
+            <SafeAreaView style={styles.container}>
+                <CarouselCards/>
+            </SafeAreaView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    homeContainer:{
+    homeContainer: {
         padding: 16,
         backgroundColor: '#F0F1F9FF',
         flex: 1
-    }
+    },
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+        // padding: 50
+    },
 })
