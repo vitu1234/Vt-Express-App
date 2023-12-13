b83ae4d7f81333bcddb26cf16ae14f0e9d117bb4
vitu1234 <mafeniblessed@gmail.com>
2023-12-14 03:39:09 +0900

    added the new slider library

--- components/Home/carousel/CarouselCards.js
+++ components/Home/carousel/CarouselCards.js
@@ -1,52 +1,21 @@
-import React from 'react'
-import {View} from "react-native"
-import Carousel, {Pagination} from 'react-native-snap-carousel'
-import CarouselCardItem, {SLIDER_WIDTH, ITEM_WIDTH} from './CarouselCardItem'
-import data from './data'
+import {Text, View} from "react-native";
+import { SliderBox } from "react-native-image-slider-box";
 
 const CarouselCards = () => {
-    const isCarousel = React.useRef(null)
-    const [index, setIndex] = React.useState(0)
-
+    const images = [
+        "https://source.unsplash.com/1024x768/?nature",
+        "https://source.unsplash.com/1024x768/?water",
+        "https://source.unsplash.com/1024x768/?girl",
+        "https://source.unsplash.com/1024x768/?tree", // Network image
+        require('../../../assets/adaptive-icon.png'),          // Local image
+    ]
 
     return (
         <View>
-            <Carousel
-                autoplay={true}
-                autoplayInterval={5000}
+            <SliderBox images={images} />
 
-                layout="default"
-                layoutCardOffset={18}
-                ref={isCarousel}
-                data={data}
-                renderItem={CarouselCardItem}
-                sliderWidth={SLIDER_WIDTH}
-                itemWidth={ITEM_WIDTH}
-                onSnapToItem={(index) => setIndex(index)}
-                useScrollView={true}
-
-
-            />
-            <Pagination
-                dotsLength={data.length}
-                activeDotIndex={index}
-                containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
-                dotStyle={{
-                    width: 10,
-                    height: 10,
-                    borderRadius: 5,
-                    marginHorizontal: 8,
-                    backgroundColor: 'rgba(255, 255, 255, 0.92)'
-                }}
-                inactiveDotStyle={{
-                    // Define styles for inactive dots here
-                }}
-                inactiveDotOpacity={0.4}
-                inactiveDotScale={0.6}
-            />
         </View>
-    )
-}
+    );
+};
 
-
-export default CarouselCards
+export default CarouselCards;
